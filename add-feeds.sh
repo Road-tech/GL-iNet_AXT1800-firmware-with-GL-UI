f19599a3a368e58f88d0d670ba3c2ed3361a695a
Road <496814664@qq.com>
2022-07-25 11:57:26 +0800

    Update add-feeds.sh

--- add-feeds.sh
+++ add-feeds.sh
@@ -20,9 +20,6 @@
 git clone https://github.com/vernesong/OpenClash.git $GITHUB_WORKSPACE/OpenClash
 cp -r $GITHUB_WORKSPACE/OpenClash/luci-app-openclash $GITHUB_WORKSPACE/gl-infra-builder/feeds/glinet/luci-app-openclash
 
-git clone https://github.com/luochongjun/istore.git $GITHUB_WORKSPACE/istore
-cp -r $GITHUB_WORKSPACE/istore/luci/luci-app-store $GITHUB_WORKSPACE/gl-infra-builder/feeds/glinet/luci-app-store
-
 git clone https://github.com/NueXini/NueXini_Packages.git $GITHUB_WORKSPACE/gl-infra-builder/feeds/NueXini
 
 # pushd wlan-ap/openwrt
